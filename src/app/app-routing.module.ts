651e610a6e31ee40463e67091bd354834e913483
S. M. JAHANGIR <smjxpro@gmail.com>
2021-02-04 21:17:44 +0600

    added auth guard
    Signed-off-by: S. M. JAHANGIR <smjxpro@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthComponent} from './auth/auth.component';
 import {RegistrationComponent} from './auth/registration/registration.component';
 import {LoginComponent} from './auth/login/login.component';
+import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './auth/guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/auth/register', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {
     path: 'auth', component: AuthComponent,
     children: [
       {path: 'register', component: RegistrationComponent},
       {path: 'login', component: LoginComponent},
     ]
-  }
+  },
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
 import {LoginComponent} from './auth/login/login.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     RegistrationComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
